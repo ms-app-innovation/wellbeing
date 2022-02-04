9028fb4806c96f1f486ba4aa5039e945cc134641
Suhas Rao <susudhee@microsoft.com>
2022-02-04 13:24:27 +1100

    Function key access changed to anonymous

--- src/Sample1/Wellbeing.API/Wellbeing.cs
+++ src/Sample1/Wellbeing.API/Wellbeing.cs
@@ -30,8 +30,9 @@ namespace Wellbeing.API
         [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(Parameters), Description = "Parameters", Required = true)]
 
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
